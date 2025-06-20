09af7993b625df9e9f9c89cc3fe4b73f0dc11fd6
Jesson Grajales <Jasongrajales31@gmail.com>
2025-06-20 18:12:50 -0500

    feat: update dependencies and db configuration

--- weatherSite/settings.py
+++ weatherSite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,9 +100,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         # Opcional:
-        'OPTIONS': {
-            'auth_plugin': 'mysql_native_password',
-        }
+        
     }
 }
 
