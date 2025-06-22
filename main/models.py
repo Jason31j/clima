13982d43890cb3e5f9bea5ee311dd57830e19edc
Jesson Grajales <Jasongrajales31@gmail.com>
2025-06-22 00:50:24 -0500

    fix: tipo de campo

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Weather(models.Model):
-    dia = models.CharField(max_length=100)
+    dia = models.IntegerField(max_length=100)
     mes = models.IntegerField()
     agno = models.IntegerField()
     probabilidad = models.IntegerField()
